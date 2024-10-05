c77e9ed219c425f563c8d8c6c8b0a73d89b22055
jyaaan <john.yamashiro@gmail.com>
2024-10-04 23:33:17 -0700

    wrapping up ch2

--- ch_02/exercises.py
+++ ch_02/exercises.py
@@ -1,3 +1,4 @@
+# type: ignore
 inputs = [1, 2, 3, 2.5]
 weights = [
     [0.2, 0.8, -0.5, 1],
@@ -29,4 +30,28 @@ print(f"{outputs=}")
 # now do it for the entire layer
 print(f"{np.dot(weights, inputs)=}")
 layer_outputs = np.dot(weights, inputs) + biases
+print(f"{layer_outputs=}")
+
+# a row vector with 3 values in numpy
+np.array([[1, 2, 3]])  # note double brackets
+# equivalent to
+a = [1, 2, 3]
+a_row = np.array([a])
+
+# which is also equivalent to
+np.expand_dims(np.array(a), axis=0)
+
+# transposing a list into a row vector into a column vector
+b = [2, 3, 4]
+b_col = np.array([b]).T
+
+np.dot(a_row, b_col)  # >>> array([[20]])
+
+# performing matrix multiplication on a sample set and 3 neurons
+inputs = [[1.0, 2.0, 3.0, 2.5], [2.0, 5.0, -1.0, 2.0], [-1.5, 2.7, 3.3, -0.8]]
+
+weights = [[0.2, 0.8, -0.5, 1.0], [0.5, -0.91, 0.26, -0.5], [-0.26, -0.27, 0.17, 0.87]]
+
+biases = [2.0, 3.0, 0.5]
+layer_outputs = np.dot(inputs, np.array(weights).T) + biases
 print(f"{layer_outputs=}")
